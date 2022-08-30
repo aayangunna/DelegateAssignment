100388ce7c693f13e1c207357eb5986aa2626ab1
aayangunna <88987151+aayangunna@users.noreply.github.com>
2022-08-30 13:09:36 +0100

    Add project files.

---
+++ DelegateAssignment/Delegate 2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class LongRunningTask
+    {
+        public delegate void Multiples(string number);
+
+        public double MultiplesOf(Multiples num)
+        {
+
+            int total = 0;
+            try
+            {
+                for (int i = 2; i < 10000000; i++)
+                {
+                    if ((i % 2) == 0)
+                    {
+                        total += i;
+                        ;
+                        num(total.ToString());
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"Found {e.Message} exception during counting");
+            }
+            return total;
+        }
+
+    }
+
+
+    /// <summary>
+    /// This is the callback class
+    /// </summary>
+    class CallBack
+    {
+        public void ShowCount()
+        {
+            var totalMultiplesOf = new LongRunningTask();
+
+            var total = totalMultiplesOf.MultiplesOf(x => Console.WriteLine($"It has found {x} multiples of 3"));
+
+        }
+    }
+}
+
+
---
+++ DelegateAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// Creating a Predicate Class
+    /// </summary>
+    internal class Delegates1
+    {
+        public delegate bool Predicate(XClass x);
+
+        void LambdaExpansion()
+        {
+
+            var UsedName = "Messi";
+
+            Predicate classwork = (XClass x) =>
+            {
+                if (x.UserName == UsedName)  //check if Username is equal to UsedName
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+
+
+    
+    // Property class
+    
+    class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
